05e2bdacf45f7e81bb07107c087d0400b6da920f
Yun Ji <this.jiyun@gmail.com>
2023-08-27 19:41:26 +0200

    mock sys.argv

--- 00_ccwc/ccwc_test.py
+++ 00_ccwc/ccwc_test.py
@@ -1,10 +1,17 @@
 import unittest
 import ccwc
+import sys
+from unittest.mock import patch
+
 
 class Test_word_count(unittest.TestCase):
     
     def test1(self):
-        self.assertEqual('foo'.upper(),'OO')
+        arg1=['-c','test.txt']
+        with patch.object(sys,'argv',arg1):
+            self.assertEqual('-c',sys.argv[0]) 
+        
+        
 
 
     
