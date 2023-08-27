65705261a0c3d0cb63221a75c12a53f69a6b1852
Yun Ji <this.jiyun@gmail.com>
2023-08-27 19:29:15 +0200

    set up test file

---
+++ 00_ccwc/ccwc_test.py
@@ -0,0 +1,13 @@
+import unittest
+import ccwc
+
+class Test_word_count(unittest.TestCase):
+    
+    def test1(self):
+        self.assertEqual('foo'.upper(),'OO')
+
+
+    
+if __name__=='__main__':
+    unittest.main()
+    
