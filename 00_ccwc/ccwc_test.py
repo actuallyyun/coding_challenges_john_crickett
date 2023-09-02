5486bbd2d0a515fc60c9d92ca09a10aa7eb5c139
Yun Ji <this.jiyun@gmail.com>
2023-09-02 12:54:26 +0200

    fix test false failure by removing extra space in output strings

--- 00_ccwc/ccwc_test.py
+++ 00_ccwc/ccwc_test.py
@@ -3,46 +3,46 @@ import subprocess
 
 class Test_word_count(unittest.TestCase):
 
-    def test_byte_count_success(self):
+    def test_bytes_count_success(self):
         
         wc_arg=['wc','-c','test.txt']
         ccwc_arg=['python','ccwc.py','-c','test.txt']
 
         wc_result=subprocess.run(wc_arg,stdout=subprocess.PIPE)
-        wc_output=wc_result.stdout.decode('utf-8')
+        wc_output=wc_result.stdout.decode('utf-8').strip()
 
         ccwc_result=subprocess.run(ccwc_arg,stdout=subprocess.PIPE)
-        ccwc_output=ccwc_result.stdout.decode('utf-8')
+        ccwc_output=ccwc_result.stdout.decode('utf-8').strip()
 
         self.assertEqual(wc_output,ccwc_output) 
     
-    def test_word_count_success(self):
+    def test_words_count_success(self):
 
         wc_arg=['wc','-w','test.txt']
         ccwc_arg=['python','ccwc.py','-w','test.txt']
 
-        wc_output=subprocess.run(wc_arg,stdout=subprocess.PIPE).stdout.decode('utf-8')
-        ccwc_output=subprocess.run(ccwc_arg,stdout=subprocess.PIPE).stdout.decode('utf-8')
+        wc_output=subprocess.run(wc_arg,stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+        ccwc_output=subprocess.run(ccwc_arg,stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
 
         self.assertEqual(wc_output,ccwc_output) 
 
-    def test_line_count_success(self):
+    def test_lines_count_success(self):
 
         wc_arg=['wc','-l','test.txt']
         ccwc_arg=['python','ccwc.py','-l','test.txt']
 
-        wc_output=subprocess.run(wc_arg,stdout=subprocess.PIPE).stdout.decode('utf-8')
-        ccwc_output=subprocess.run(ccwc_arg,stdout=subprocess.PIPE).stdout.decode('utf-8')
+        wc_output=subprocess.run(wc_arg,stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+        ccwc_output=subprocess.run(ccwc_arg,stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
 
         self.assertEqual(wc_output,ccwc_output) 
 
-    def test_character_count_success(self):
+    def test_characters_count_success(self):
 
         wc_arg=['wc','-m','test.txt']
         ccwc_arg=['python','ccwc.py','-m','test.txt']
 
-        wc_output=subprocess.run(wc_arg,stdout=subprocess.PIPE).stdout.decode('utf-8')
-        ccwc_output=subprocess.run(ccwc_arg,stdout=subprocess.PIPE).stdout.decode('utf-8')
+        wc_output=subprocess.run(wc_arg,stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+        ccwc_output=subprocess.run(ccwc_arg,stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
 
         self.assertEqual(wc_output,ccwc_output) 
 
@@ -50,8 +50,8 @@ class Test_word_count(unittest.TestCase):
         wc_arg=['wc','test.txt']
         ccwc_arg=['python','ccwc.py','test.txt']
 
-        wc_output=subprocess.run(wc_arg,stdout=subprocess.PIPE).stdout.decode('utf-8')
-        ccwc_output=subprocess.run(ccwc_arg,stdout=subprocess.PIPE).stdout.decode('utf-8')
+        wc_output=subprocess.run(wc_arg,stdout=subprocess.PIPE).stdout.decode('utf-8').strip().replace(" ","")
+        ccwc_output=subprocess.run(ccwc_arg,stdout=subprocess.PIPE).stdout.decode('utf-8').strip().replace(" ","")
 
         self.assertEqual(wc_output,ccwc_output) 
 
